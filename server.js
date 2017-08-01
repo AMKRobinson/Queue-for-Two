222e411342987cf657fc8ea66dcb20bf5c43971b
KatherineHanson <katherinemariehanson@gmail.com>
2017-08-01 11:28:55 -0700

    added SQL documentation

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 'use strict';
 
+// SQL query to create media table
+
+// CREATE TABLE IF NOT EXISTS Media (
+// media_id SERIAL PRIMARY KEY,
+// url_string text
+// );
+
+
 //SQL query to create customer table.
 
 // CREATE TABLE IF NOT EXISTS Customer (
-// customerId SERIAL PRIMARY KEY,
+// customer_id SERIAL PRIMARY KEY,
 // username VARCHAR(100),
 // password VARCHAR(100),
 // name VARCHAR(255),
 // email VARCHAR(255)
 // );
 
-// SQL Query to create show table
 
-// CREATE TABLE IF NOT EXISTS Show (
-// showId SERIAL PRIMARY KEY,
-// urlString text
+// SQL query to insert a row into Media
+
+// INSERT INTO Media
+// (url_string)
+// VALUES ('https://www.themoviedb.org/tv/1399-game-of-thrones'
 // );
 
-// SQL query to create movie table
-// CREATE TABLE IF NOT EXISTS Movie (
-// movieId SERIAL PRIMARY KEY,
-// urlString text
-// );
+// SQL query to insert a row into Customer
+// INSERT INTO Customer
+// (username, password, name, email)
+// VALUES ('carrieH','lilies','Carrie Hans', 'carriehans@gmail.com');
